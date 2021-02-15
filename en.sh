f047b40a75eb876eb9e7ed7c3ff6540b05f79347
Antibiotics <abx@abx.pe.kr>
2021-02-15 21:41:52 +0900

    2021-02-15
    Default Setting Available

---
+++ en.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+NoticeStart="
+############# BASH Russian Roulette For Linux #############
+\n###########################################################
+\n###########################################################
+\n###########################################################
+"
+
+SelMag="
+\n\n### Select Game Setting ###
+\n\n### 1. Default Setting (Recommended)
+\n### 2. Advanced (Coming soon)
+"
+
+GameStart="
+\n### The game has just started.
+\n### It's time to check how lucky you are...
+
+"
+
+SelAction="
+\n
+\n### 1. (Enter any key) Pull Trigger
+\n### 2. Stop Game
+"
+
+BulletFired="Bang!"
+
+BulletNotFired="Click"
