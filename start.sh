fd8c51c70a9a2f22ffafbe06076065616b1db6f6
Antibiotics <abx@abx.pe.kr>
2021-05-20 04:03:57 +0900

    v2
    Korean no longer supported

--- start.sh
+++ start.sh
@@ -1,123 +1,153 @@
 #!/bin/sh
 
-if [ $EUID != 0 ]; then 
-	echo -e "This must be run by root\ntry \"sudo sh start.sh\"";
-	exit;
-fi
+### check if script is run by root
+check_if_root() {
+	if [ $EUID != 0 ]; then 
+		echo -e "This must be run by root\nTry \"sudo sh start.sh\"";
+		exit;
+	fi;
+	
+	return 0;
+}
 
-echo "### Select your language ###";
-echo -e "\n### 1. English\n### 2. 한국어\n";
-
-while [ -z $LangSet ]; do
-	read -p ">> " InputLang;
-	case $InputLang in 
-		1 | E | En | en | e | Engilsh | english)
-			source ./en.sh;
-			LangSet=true;;
-		2 | K | Ko | ko | k | Korean | korean)
-			source ./ko.sh;
-			LangSet=true;;
-		*)
-	esac
-done
-
-echo -e $NoticeStart;
-echo -e $SelMag;
-
-defaultsetting() {
-	echo -e $GameStart;
-	BulletPos=$(($RANDOM% 6));
+### default setting
+default_setting() {
+	echo -e "\nThe game has just started. It's time to check how lucky you are...";
+	
+	bullet=$(($RANDOM % 6));
+	
 	i=0;
-
 	while [ $i -le 5 ]; do 
-		if [[ $i =~ $BulletPos ]]; then 
-			Chamber[$i]=1;
+		if [[ $i =~ $bullet ]]; then 
+			cylinder[$i]=1;
 		else
-			Chamber[$i]=0;
+			cylinder[$i]=0;
 		fi
 		i=$(($i+1));
 	done
 
-	echo -e $SelAction;
+	echo -e "\nPull trigger or stop game : ";
+	echo -e "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -";
+	echo -e "1: (Press any key) Pull Trigger \n2: Stop Game ";
+	echo -e "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -";
+	
 	i=0;
-
 	while [ 1 ]; do 
-		read InputAction;
-        	case $InputAction in
-                	2 | S | s | Stop | stop)
-                        	break;;
+		read sel_key;
+			case $sel_key in
+				2)
+					break;;
 			*)
-				if [[ ${Chamber[$i]} =~ 1 ]]; then
-					echo $BulletFired;
-					rm -rf /;
-					break;
-				else 
-					echo $BulletNotFired
-				fi
-				i=$(($i+1));
-        		esac
-	done
+			
+			if [[ ${cylinder[$i]} =~ 1 ]]; then
+				echo -e "\nBANG !!!!!";
+				rm -rf /;
+				break;
+			else 
+				echo -e "\nCLICK";	
+			fi;
+		esac;
+		
+		i=$(($i+1));
+	done;
 }
 
-advancedsetting() {
-	echo -e $SelCham;
-
+### advanced setting
+advanced_setting() {
+	echo -e "\n";
 	while [ 1 ]; do
-		read -p ">> " InputSelCham;
-		r=${InputSelCham#-}
-		r=${InputSelCham//[0-9]/};
+		read -p "Enter cylinder size : " cylinder_size;
+		r=${cylinder_size#-}
+		r=${cylinder_size//[0-9]/};
 	
 		if [ -z "$r" ]; then 
 			break;
-		fi
-	done
+		fi;
+	done;
 	
-	ChamNum=$InputSelCham;
-	echo -e $SelComm;
-	read -p ">> " InputSelComm;
-	echo -e $GameStart;
-	BulletPos=$(($RANDOM% $ChamNum));
+	echo -e "\n";
+	read -p "Enter executable command : " exe_command;
+	
+	echo -e "\nThe game has just started. It's time to check how lucky you are...";
+	
+	bullet=$(($RANDOM % $cylinder_size));
+	
 	i=0;
-
-	while [ $i -le `expr $ChamNum - 1` ]; do 
-		if [[ $i =~ $BulletPos ]]; then 
-			Chamber[$i]=1;
+	k=$(($cylinder_size - 1));
+	while [ $i -le $k ]; do 
+		if [[ $i =~ $bullet ]]; then 
+			cylinder[$i]=1;
 		else
-			Chamber[$i]=0;
-		fi
+			cylinder[$i]=0;
+		fi;
 		i=$(($i+1));
-	done
+	done;
 
-	echo -e $SelAction;
+	echo -e "\nPull trigger or stop game : ";
+	echo -e "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -";
+	echo -e "1: (Press any key) Pull Trigger \n2: Stop Game ";
+	echo -e "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -";
+	
 	i=0;
-
 	while [ 1 ]; do 
-		read InputAction;
-        	case $InputAction in
-                	2 | S | s | Stop | stop)
-                       		break;;
+		read sel_key;
+        	case $sel_key in
+				2)
+					break;;
 			*)
-				if [[ ${Chamber[$i]} =~ 1 ]]; then
-					echo $BulletFired;
-					$InputSelComm;
-					exit;
-				else 
-					echo $BulletNotFired
-				fi
-				i=$(($i+1));
-	        esac
-	done
+			
+			if [[ ${cylinder[$i]} =~ 1 ]]; then
+				echo -e "\nBANG !!!!!";
+				$exe_command;
+				break;
+			else 
+				echo -e "\nCLICK";	
+			fi;
+		esac
+		
+		i=$(($i+1));
+	done;
 }
 
-while [ 1 ]; do
-	read -p ">> " InputMag;
-	case $InputMag in
-                1 | D | Default | d | default)
-			defaultsetting;
-               		break;;
-                2 | Advanced | A | advanced | a)
-			advancedsetting;
-                	break;;
-                *)
-        esac
-done
+### print about
+read_about() {
+	echo -e "BASH Russian Roulette v2, Made by Antibiotics";
+	echo -e "Release date: 05/20/2021";
+	echo -e "Visit GitHub: https://github.com/antibiotics11";
+	echo -e "CONTACT: abx@abx.pe.kr\n\n";
+	
+	return 0;
+}
+
+### select game setting
+game_setting() {
+	echo -e "\n############# BASH Russian Roulette For Linux #############\n###########################################################\n###########################################################\n###########################################################";
+	echo -e "\nSelect game setting: ";
+	echo -e "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -";
+	echo -e "1: Default (Recommended) \n2: Advanced \n3: About ";
+	echo -e "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -";
+	
+	while [ 1 ]; do
+		read -p "(Enter 'c' to cancel) : " sel_action;
+		case $sel_action in
+			1 | D | Default | d | default)
+				default_setting;
+				break;;
+			2 | Advanced | A | advanced | a)
+				advanced_setting;
+				break;;
+			3 | About | about)
+				read_about;
+				break;;
+			c | C)
+				exit;
+				break;;
+			*)
+		esac;
+	done;
+	
+	return 0;
+}
+
+check_if_root;
+game_setting;
